7948aad3d0e94b29686b1696e013b15f9f4f736e
Quark <72112478+QUARK88@users.noreply.github.com>
2021-05-03 16:07:01 -0400

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -2,12 +2,56 @@ ideologies = [
     {
         "name": "Centrism",
         "stats": {
-            "b1": 50,
+            "b1": 45,
             "b2": 50,
             "b3": 50,
-            "b4": 50,
+            "b4": 55,
             "b5": 50,
             "b6": 50,
+        }
+    },
+    {
+        "name": "Georgism",
+        "stats": {
+            "b1": 50,
+            "b2": 30,
+            "b3": 40,
+            "b4": 70,
+            "b5": 65,
+            "b6": 40,
+        }
+    },
+    {
+        "name": "Anarcho-Primitivism",
+        "stats": {
+            "b1": 75,
+            "b2": 50,
+            "b3": 0,
+            "b4": 60,
+            "b5": 60,
+            "b6": 80,
+        }
+    },
+    {
+        "name": "State Liberalism",
+        "stats": {
+            "b1": 20,
+            "b2": 35,
+            "b3": 85,
+            "b4": 40,
+            "b5": 40,
+            "b6": 65,
+        }
+    },
+    {
+        "name": "New Right Conservatism",
+        "stats": {
+            "b1": 15,
+            "b2": 35,
+            "b3": 55,
+            "b4": 40,
+            "b5": 40,
+            "b6": 65,
         }
     }
 ];
