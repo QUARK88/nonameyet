34b7830f13c19bc7cfdcafc27bd82593ad0c8d5b
Quark <72112478+QUARK88@users.noreply.github.com>
2021-05-03 08:29:22 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -264,88 +264,11 @@ questions = [
         }
     },
     {
-        "question": "Local governments should be more powerful than national governments.",
+        "question": "The populous cannot govern themselves.",
         "effect": {
             "b1": 0,
             "b2": 0,
             "b3": 1,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "Citizens, not bureaucrats, should hold political power.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": 1,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "Nobody should have access to my private information without my consent.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": 1,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "Power corrupts, and absolute power corrupts absolutely.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": 1,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "The state should be abolished.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": 2,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "Like minded individuals should form self governing communities.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": 2,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "The vast majority of people cannot govern themselves.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": -1,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "It is important for us to maintain a strong military.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": -1,
             "b4": 0,
             "b5": 0,
             "b6": 0,
@@ -356,7 +279,7 @@ questions = [
         "effect": {
             "b1": 0,
             "b2": 0,
-            "b3": -1,
+            "b3": 1,
             "b4": 0,
             "b5": 0,
             "b6": 0,
@@ -367,18 +290,7 @@ questions = [
         "effect": {
             "b1": 0,
             "b2": 0,
-            "b3": -1,
-            "b4": 0,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
-        "question": "Only a select few can be trusted with political power.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": -2,
+            "b3": 1,
             "b4": 0,
             "b5": 0,
             "b6": 0,
@@ -389,6 +301,83 @@ questions = [
         "effect": {
             "b1": 0,
             "b2": 0,
+            "b3": 2,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Life without the state would be nasty, brutish and short.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 2,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Local governments should be more powerful than national governments.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Citizens, not officials, should hold political power.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Nobody should have access to my private information without my consent.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Power corrupts, and absolute power corrupts absolutely",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "We should only maintain a military for self defence.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "The state should be abolished.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
             "b3": -2,
             "b4": 0,
             "b5": 0,
@@ -396,7 +385,18 @@ questions = [
         }
     },
     {
-        "question": "There should be no restrictions on abortion.",
+        "question": "Like minded individuals should form self governing communities.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -2,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "If people want to live unhealthy lifestyles, it is their right to do so.",
         "effect": {
             "b1": 0,
             "b2": 0,
@@ -440,17 +440,6 @@ questions = [
         }
     },
     {
-        "question": "Any sexual activity is fine as long as everyone involved is a consenting adult.",
-        "effect": {
-            "b1": 0,
-            "b2": 0,
-            "b3": 0,
-            "b4": 2,
-            "b5": 0,
-            "b6": 0,
-        }
-    },
-    {
         "question": "An individual should be allowed to do whatever they want with their body.",
         "effect": {
             "b1": 0,
@@ -462,18 +451,18 @@ questions = [
         }
     },
     {
-        "question": "It should be hard for the average person to acquire a weapon.",
+        "question": "Any sexual activity is fine as long as everyone involved is a consenting adult.",
         "effect": {
             "b1": 0,
             "b2": 0,
             "b3": 0,
-            "b4": -1,
+            "b4": 2,
             "b5": 0,
             "b6": 0,
         }
     },
     {
-        "question": "We shouldn’t let people do things which will put them in danger.",
+        "question": "Pornography should be banned.",
         "effect": {
             "b1": 0,
             "b2": 0,
@@ -495,6 +484,17 @@ questions = [
         }
     },
     {
+        "question": "The worst criminals should be publicly executed.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
         "question": "An individual can never be truly separate from society.",
         "effect": {
             "b1": 0,
@@ -506,7 +506,7 @@ questions = [
         }
     },
     {
-        "question": "The worst criminals should be publicly executed.",
+        "question": "We shouldn’t let people do things which will put them in danger.",
         "effect": {
             "b1": 0,
             "b2": 0,
@@ -789,6 +789,336 @@ questions = [
             "b4": 0,
             "b5": 0,
             "b6": -2,
+        }
+    },
+    {
+        "question": "Sometimes vigilante justice is necessary.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Trespassing should be met with harsh consequences.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "The value of art is determined by how well it sells.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Hate speech against minorities should be a crime.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "There should be laws against wearing religious clothing in public.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": -1,
+            "b5": 0,
+            "b6": -1,
+        }
+    },
+    {
+        "question": "Firearms should be available to the general public.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 1,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "People should be allowed to use their property however they please.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Prostitution should be allowed.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Gay marriage should be legal",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "It’s often necessary to break taboos.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 1,
+            "b5": 0,
+            "b6": -1,
+        }
+    },
+    {
+        "question": "A singular world government is desirable.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Local councils should have more power to implement economic policies.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Hierarchies should be dismantled.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Monarchy is a good system of government.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "The nation comes before all else.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "The state should control all aspects of the economy.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "A party of revolutionaries should lead the workers against capitalism.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Scientists and experts should rule instead of elected politicians.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": -1,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+        }
+    },
+    {
+        "question": "Feudalism was not such a bad system.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "Space travel should not be funded by tax money.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "It would be a shame to see minority languages go extinct.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "Nomadic cultures have a wiser relationship with the land than we do.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 1,
+        }
+    },
+    {
+        "question": "Shops should not be required to close on Sundays or during religious festivals.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": -1,
+        }
+    },
+    {
+        "question": "Gender should be abolished.",
+        "effect": {
+            "b1": 0,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": 1,
+            "b6": -1,
+        }
+    },
+    {
+        "question": " The wealth disparity between different ethnic groups is caused by some groups working harder than others.",
+        "effect": {
+            "b1": -1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Workers in essential parts of the economy should not be allowed to strike.",
+        "effect": {
+            "b1": -1,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Powerful bankers manipulate our society behind the scenes.",
+        "effect": {
+            "b1": 1,
+            "b2": 0,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Communism is a desirable end goal.",
+        "effect": {
+            "b1": 1,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": 0,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "We should increase tariffs on imports in order to help local producers.",
+        "effect": {
+            "b1": 0,
+            "b2": 1,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
+        }
+    },
+    {
+        "question": "Welfare for disabled people should be cut.",
+        "effect": {
+            "b1": 0,
+            "b2": -1,
+            "b3": 0,
+            "b4": 0,
+            "b5": -1,
+            "b6": 0,
         }
     }
 ];
